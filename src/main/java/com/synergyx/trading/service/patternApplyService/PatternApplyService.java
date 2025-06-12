6cd81e616a6dae0effe2704b8207ff928e217172
Soomxn <sooming@duksung.ac.kr>
2025-06-12 21:06:35 +0900

    [FEAT] 패턴 적용 정보 - 종목 엔티티 연결 #3

--- src/main/java/com/synergyx/trading/service/patternApplyService/PatternApplyService.java
+++ src/main/java/com/synergyx/trading/service/patternApplyService/PatternApplyService.java
@@ -5,8 +5,8 @@ import com.synergyx.trading.dto.patternApply.PatternApplyResponseDTO;
 
 public interface PatternApplyService {
     // 패턴 적용
-    PatternApplyResponseDTO.PatternApplyResultDTO applyPattern(PatternApplyRequestDTO.PatternApplyDTO request);
+    PatternApplyResponseDTO.PatternApplyResultDTO applyPattern(Long userId, PatternApplyRequestDTO.PatternApplyDTO request);
 
     // 패턴 알림 토글
-    PatternApplyResponseDTO.PatternApplyResultDTO toggleNotification(Long patternApplyId);
+    PatternApplyResponseDTO.PatternApplyResultDTO toggleNotification(Long userId, Long patternApplyId);
 }

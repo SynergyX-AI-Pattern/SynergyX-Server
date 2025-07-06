653bd0edb81152f0dffd2676e0637f9bda19e07a
eldeoddt <eldeoddt@gmail.com>
2025-07-06 21:22:18 +0900

    [REFACTOR] kis token 서비스 로직 수정 #46

--- src/main/java/com/synergyx/trading/repository/KisTokenRepository.java
+++ src/main/java/com/synergyx/trading/repository/KisTokenRepository.java
@@ -3,6 +3,10 @@ package com.synergyx.trading.repository;
 import com.synergyx.trading.model.KisToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface KisTokenRepository extends JpaRepository<KisToken, Long> {
-    KisToken findTopByOrderByUpdatedAtDesc();
+    Optional<KisToken> findTopByOrderByExpiresAtDesc();
+
+    boolean existsByAccessToken(String accessToken);
 }

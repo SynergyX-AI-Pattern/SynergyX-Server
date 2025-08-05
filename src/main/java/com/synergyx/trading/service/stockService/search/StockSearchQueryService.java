e9d2421ecdff4a272661a716ec6c8bd42d21f1d7
Soomxn <sooming@duksung.ac.kr>
2025-08-05 14:12:09 +0900

    [FEAT] 이미지 기반 종목 정보 반환 서비스 구현 #69

--- src/main/java/com/synergyx/trading/service/stockService/search/StockSearchQueryService.java
+++ src/main/java/com/synergyx/trading/service/stockService/search/StockSearchQueryService.java
@@ -1,9 +1,12 @@
 package com.synergyx.trading.service.stockService.search;
 
 import com.synergyx.trading.dto.stockSearch.StockSearchResponseDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface StockSearchQueryService {
     List<StockSearchResponseDTO> searchStocksByName(String keyword);
+    // 이미지 기반 검색
+    StockSearchResponseDTO searchStockByImage(MultipartFile image);
 }

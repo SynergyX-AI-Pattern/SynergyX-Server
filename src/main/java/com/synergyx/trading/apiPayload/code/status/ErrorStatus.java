2e6064e1407b4ec08b0f717ceedc5d1f8e1e7f5b
Soomxn <sooming@duksung.ac.kr>
2025-09-01 17:00:32 +0900

    [FEAT] 에러 코드 추가 #83

--- src/main/java/com/synergyx/trading/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/synergyx/trading/apiPayload/code/status/ErrorStatus.java
@@ -43,6 +43,7 @@ public enum ErrorStatus implements BaseErrorCode {
     BACKTEST_NOT_FOUND(HttpStatus.NOT_FOUND, "BACKTEST404", "해당 백테스트 결과가 존재하지 않습니다."),
     BACKTEST_PERIOD_EXCEEDS_LIMIT(HttpStatus.BAD_REQUEST, "BACKTEST400", "백테스트 기간은 최대 5년까지 가능합니다."),
     FASTAPI_BACKTEST_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "BACKTEST502", "FastAPI 백테스트 서버 호출에 실패했습니다."),
+    HIGHLIGHT_RANGE_NOT_FOUND(HttpStatus.BAD_REQUEST, "BACKTEST401", "해당 백테스트 결과에 하이라이트 구간이 존재하지 않습니다."),
 
     // 캔들 데이터 관련 예외 처리
     CANDLE_DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "CANDLE404", "캔들 데이터가 존재하지 않습니다."),

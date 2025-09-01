730f482dd538cb114f1ead34403c75fbcd99798e
Soomxn <sooming@duksung.ac.kr>
2025-09-01 17:09:50 +0900

    [FEAT] 종목 repo에 기간별 시간봉 조회 로직 추가 #83

--- src/main/java/com/synergyx/trading/repository/StockOhlcv1hRepository.java
+++ src/main/java/com/synergyx/trading/repository/StockOhlcv1hRepository.java
@@ -4,11 +4,17 @@ import com.synergyx.trading.model.StockOhlcv1h;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface StockOhlcv1hRepository extends JpaRepository<StockOhlcv1h, Long> {
 
     // 중복 방지용
     Optional<StockOhlcv1h> findByStock_IdAndTimestamp(Long stockId, LocalDateTime timestamp);
+
+    // 기간별 시간봉 조회 (오름차순 정렬)
+    List<StockOhlcv1h> findByStockIdAndTimestampBetweenOrderByTimestampAsc(
+            Long stockId, LocalDateTime start, LocalDateTime end
+    );
 }
 

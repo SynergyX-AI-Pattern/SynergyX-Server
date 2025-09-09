bf3eb90847b04b0eb406c07ac0586b1e563f0dc5
Soomxn <sooming@duksung.ac.kr>
2025-09-09 21:38:13 +0900

    [FEAT] 이미지 종목 검색 서비스 구현 #80

--- src/main/java/com/synergyx/trading/service/stockService/search/StockSearchQueryService.java
+++ src/main/java/com/synergyx/trading/service/stockService/search/StockSearchQueryService.java
@@ -1,5 +1,6 @@
 package com.synergyx.trading.service.stockService.search;
 
+import com.synergyx.trading.dto.stockSearch.StockSearchFastApiResponseDTO;
 import com.synergyx.trading.dto.stockSearch.StockSearchResponseDTO;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 public interface StockSearchQueryService {
     List<StockSearchResponseDTO> searchStocksByName(String keyword);
+
     // 이미지 기반 검색
-    StockSearchResponseDTO searchStockByImage(MultipartFile image);
+    StockSearchFastApiResponseDTO.StockSearchFastApiInfoResponseDTO searchStockByImage(MultipartFile image);
 }
